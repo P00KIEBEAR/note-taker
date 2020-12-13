42467f4fee11cccd67c17618f732caf29b8f3f5f
pat paggi <patpaggi@gmail.com>
2020-12-13 12:23:28 -0700

    add

--- db/helperFile.js
+++ db/helperFile.js
@@ -1,6 +1,17 @@
+const { notes } = require('./db');
+const fs = require('fs');
+const path = require('path');
 
+const getNotes = (body, notes) => {
 
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db.json'),
+    JSON.stringify({ notes: notes }, null, 2)
+  );
+  return note;
 
-const getNotes = () => {
-  console.log('hi')
 }
+
+module.exports = getNotes
--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,32 @@
 const router = require('express').Router();
 const { response } = require('express');
-const db = require('../db/helperFile');
+const notes = require('../db/db.json');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+uuidv4();
 
-/*
+
 router.get('/notes', (req, res) => {
-  db.getNotes()
-    .then((notes) => res.json(notes))
-    .catch((err) => res.status(500).json(err))
 
-})*/
+  res.json(notes)
+});
+
+
+
+router.post('/notes', (req, res) => {
+  //db.getNotes(body, notes)
+  const note = req.body
+  notes.push(note)
+  fs.writeFileSync(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify(notes, null, 2)
+  );
+
+  res.json(notes)
+  // .catch((err) => res.status(500).json(err))
+
+})
 
 
 module.exports = router
